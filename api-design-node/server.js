fce3f9db5a2f73a54436e3fd108b3be9d59e7632
Rod Machen <mail@rodmachen.com>
2016-03-07 21:06:39 -0600

    Finish server.js

--- api-design-node/server.js
+++ api-design-node/server.js
@@ -9,11 +9,11 @@ var app = express();
 var jsonData = {count: 12, message: 'hey'};
 
 app.get('/', function(req, res) {
-	fs.readFile('/index.html', function(err, data) {
+	fs.readFile('./index.html', 'utf8', function(err, data) {
 		if (err) {
 			throw err;
 		} else {
-			console.log(data);
+			res.send(data);
 		}
 	});
 });
@@ -22,4 +22,6 @@ app.get('/data', function(req, res) {
 	res.json(jsonData);
 });
 
-app.listen('3000')
+app.listen('3000', function() {
+	console.log('Example app listening on port 3000!');
+});
