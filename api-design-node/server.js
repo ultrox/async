a25e07086efcf605d2abfdb63ef183ebd9839b79
Rod Machen <mail@rodmachen.com>
2016-03-07 21:21:45 -0600

    Optimize server.js

--- api-design-node/server.js
+++ api-design-node/server.js
@@ -9,12 +9,8 @@ var app = express();
 var jsonData = {count: 12, message: 'hey'};
 
 app.get('/', function(req, res) {
-	fs.readFile('./index.html', 'utf8', function(err, data) {
-		if (err) {
-			throw err;
-		} else {
-			res.send(data);
-		}
+	res.sendFile(__dirname + '/index.html', function(err) {
+		res.status(500).send(err);
 	});
 });
 
