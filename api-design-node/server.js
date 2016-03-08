67b3b8186b426f87188de9a1eb0e4d791ac89656
Rod Machen <mail@rodmachen.com>
2016-03-07 20:25:52 -0600

    First attempt server.js

--- api-design-node/server.js
+++ api-design-node/server.js
@@ -2,4 +2,17 @@
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
 
+var express = require('express');
+var app = express();
+
 var jsonData = {count: 12, message: 'hey'};
+
+app.get('/', function(req, res) {
+	res.send('index.html');
+});
+
+app.get('/data', function(req, res) {
+	res.json(jsonData);
+});
+
+app.listen('3000')
