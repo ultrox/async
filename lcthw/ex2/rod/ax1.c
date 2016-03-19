df6e1ab1f9a6ed287595b7c836b5a079b2a54fa0
Rod Machen <mail@rodmachen.com>
2016-03-18 22:34:27 -0500

    Complete ex2

---
+++ lcthw/ex2/rod/ax1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* This is a comment. */
+int main(int argc, char *argv[])
+{
+	int distance = 200;
+	int length = 30;
+
+	// this is also a comment
+	printf("You are %d miles away. You are %d inches tall.\n", distance, length);
+	
+	return 0;
+}
