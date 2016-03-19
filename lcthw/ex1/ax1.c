6e39d047ce3b5623a7de801fa21373ef08b78466
Rod Machen <mail@rodmachen.com>
2016-03-18 21:40:53 -0500

    Move to 'a' syntax

--- lcthw/ex1/ax1.c
+++ lcthw/ex1/ax1.c
@@ -3,10 +3,11 @@
 /* This is a comment. */
 int main(int argc, char *argv[])
 {
-	int distance = 100;
+	int distance = 200;
+	int length = 30;
 
 	// this is also a comment
-	printf("You are %d miles away.\n", distance);
-
+	printf("You are %d miles away. You are %d inches tall.\n", distance, length);
+	
 	return 0;
 }
