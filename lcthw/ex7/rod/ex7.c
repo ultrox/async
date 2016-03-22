df04fb54bd36c6efec9b5f1e7f624e1474566127
Rod Machen <mail@rodmachen.com>
2016-03-22 12:36:03 -0500

    Finish ex7

--- lcthw/ex7/rod/ex7.c
+++ lcthw/ex7/rod/ex7.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
     int distance = 100;
     float power = 2.345f;
@@ -22,7 +22,7 @@ int main (int argc, char *argv[])
     
     printf("You have %d bugs at an imaginary rate of %f.\n", bugs, bug_rate);
 
-    long universe_of_defects = 1L * 1024L * 1024L * 1024L;
+    unsigned long universe_of_defects = 1L * 10024L * 10024L * 10024L  * 10024L * 10024L;
     printf("The entire universe has %ld bugs.\n", universe_of_defects);
 
     double expected_bugs = bugs * bug_rate;
