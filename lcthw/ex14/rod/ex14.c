52d07160bca47a773c0c0d6aa5e3c402427a052f
Rod Machen <mail@rodmachen.com>
2016-03-25 14:43:15 -0500

    Update ex14

--- lcthw/ex14/rod/ex14.c
+++ lcthw/ex14/rod/ex14.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 int can_print_it(char ch);
-void print_letters(char arg[]);
+void print_letters(char arg[], int len);
 
 void print_arguments(int argc, char *argv[])
 {
     int i = 0;
 
-    for (i = 0; i < argc; i++) {
-        print_letters(argv[i]);
+    for (i = 1; i < argc; i++) {
+        print_letters(argv[i], strlen(argv[i]));
     }
 }
 
-void print_letters(char arg[]) 
+void print_letters(char arg[], int len) 
 {
     int i = 0;
 
-    for (i = 0; arg[i] != '\0'; i++) {
+    for (i = 0; len ; i++) {
         char ch = arg[i];
 
         if (can_print_it(ch)) {
