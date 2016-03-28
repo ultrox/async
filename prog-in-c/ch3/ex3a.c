c78cbe929481310a28e35bc2ce93c758b9fd560e
Rod Machen <mail@rodmachen.com>
2016-03-27 22:21:42 -0500

    Finish ch 3

---
+++ prog-in-c/ch3/ex3a.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int sum;
+    // Compute result
+    sum = 25 + 37 - 19;
+    // Display result
+    printf("The answer is %i.\n", sum);
+
+    return 0;
+}
