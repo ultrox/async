a086809f035b302ab640776af93ebc7f089d9197
Rod Machen <mail@rodmachen.com>
2016-03-27 22:05:50 -0500

    ex3.1-3.5

---
+++ prog-in-c/ch3/ex3.4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int sum = 25 + 50;
+
+    printf("The sum of 50 and 25 is %i.\n", sum);
+
+    return 0;
+}
---
+++ prog-in-c/ch3/ex3.5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int value1 = 50;
+    int value2 = 25;
+    int sum = value1 + value2;
+
+    printf("The sum of %i and %i is %i.\n", value1, value2, sum);
+
+    return 0;
+}
