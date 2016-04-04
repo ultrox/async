52e28f0dee152a0eca39e7ce8dcc788de05906d6
Rod Machen <mail@rodmachen.com>
2016-04-04 07:21:53 -0500

    ex6.1  Please enter the commit message for your changes. Lines starting

--- prog-in-c/ch6/ex6.1.c
+++ prog-in-c/ch6/ex6.1.c
@@ -1,3 +1,16 @@
 #include <stdio.h>
 
+int main (void)
+{
+    int number;
 
+    printf("Type in your number: \n");
+    scanf("%i", &number);
+
+    if (number < 0) {
+        number = -number;
+    }
+
+    printf("The absolute value is %i\n", number);
+
+}
